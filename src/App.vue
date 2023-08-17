5ee4b3f925aba06ad26040b251b79711c8d58fb4
Ana-Maria <alcazaanamaria@gmail.com>
2023-08-17 14:23:08 +0300

    new version 0.0.105

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import Greet from "./components/Greet.vue";
 
 <template>
   <div class="container">
-    <h1>Welcome to Tauri! v0.0.74</h1>
+    <h1>Welcome to Tauri! v0.0.105</h1>
 
     <div class="row">
       <a href="https://vitejs.dev" target="_blank">
